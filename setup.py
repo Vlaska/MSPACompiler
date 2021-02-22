0266fea69b1b5f8b9b94748b997765fd2b5cbd05
Kamil Gierada <vlaska8888@gmail.com>
2021-02-22 22:07:08 +0100

    Not executable

--- setup.py
+++ setup.py
@@ -2,17 +2,12 @@ from setuptools import find_packages, setup
 
 setup(
     name='MSPACompiler',
-    version='1.2.0',
+    version='1.2.1',
     packages=find_packages(),
     include_package_data=True,
     author='Vlaska',
     author_email='vlaska8888@gmail.com',
     url='https://github.com/Vlaska/MSPACompiler',
-    install_requires=['Arpeggio', 'lupa', 'click', 'fcache', 'loguru'],
-    entry_points='''
-    [console_scripts]
-    mspac=MSPACompiler.scripts.mspac:mspac
-    mspa=MSPACompiler.scripts.mspa:cli
-    ''',
+    install_requires=['Arpeggio', 'lupa', 'fcache', 'loguru'],
     python_requires='>=3.8',
 )
